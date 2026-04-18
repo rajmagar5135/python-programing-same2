b1805024e82ed9a9e1b16fb9593964c2f56ac8d6
rajmagar5135 <magarraj45426@gmail.com>
2026-04-18 12:31:32 +0700

    Create program 1.py

---
+++ Experiment no 3/program 1.py
@@ -0,0 +1,11 @@
+# To generate a number pattern
+Created on Mon Feb 16 15:26:04 2026
+@author: Raj Magar
+n=int(input("Enter number of rows:"))
+
+
+
+for i in range(1, n + 1):
+for j in range(1, i + 1):
+print(j, end=" ")
+print()
