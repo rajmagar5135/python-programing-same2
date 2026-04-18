021dea5d622343913fcb1c7139e621c0c3db5fea
rajmagar5135 <magarraj45426@gmail.com>
2026-04-18 12:47:18 +0700

    Create program 1.py

---
+++ Experiment no 5/program 1.py
@@ -0,0 +1,28 @@
+# to implement python programs using dictionaries & sets
+Created on Mon Mar 9 14:45:31 2026
+@author: Raj Magar
+
+student = {
+
+
+
+student["name"] = "om"
+student["age"] = 19
+
+
+
+student["course"] = "Python"
+
+
+
+print("Dictionary after adding elements:")
+print(student)
+student["age"] = 21
+print("\nDictionary after updating a value:")
+print(student)
+del student["course"]
+
+
+
+print("\nDictionary after deleting a key-value pair:")
+print(student)
