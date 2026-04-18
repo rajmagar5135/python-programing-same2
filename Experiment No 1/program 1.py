23c04b5a9a59de7611744f24d63ffdfaccb9f90e
rajmagar5135 <magarraj45426@gmail.com>
2026-04-18 12:11:39 +0700

    Add program to display personal details

---
+++ Experiment No 1/program 1.py
@@ -0,0 +1,18 @@
+# Program to display personal details
+Created on Thu Apr 2 00:18:53 2026
+@author: Raj Magar
+name = "Raj Magar"
+address = "Satara, Maharashtra, India"
+
+10
+
+contact = "93784681296"
+print("name:", name)
+
+13
+
+print(" address:", address)
+
+14
+
+print("contact:", contact)
