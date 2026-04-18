e653d85bb892cee224eceeb67bb6f1fb5ad73eb4
rajmagar5135 <magarraj45426@gmail.com>
2026-04-18 12:12:35 +0700

    Add basic arithmetic operations in Act 1.py

---
+++ Experiment No 1/Act 1.py
@@ -0,0 +1,21 @@
+num1=int(input("Enter Number1:"))
+num2=int(input("Enter Numbe2r:"))
+sum=num1+num2
+
+5
+
+print("Addition=",sum)
+sub=num1-num2
+print("Substraction=",sub)
+mul=num1*num2
+
+11
+
+print("Multiplication=",mul)
+div=num1/num2
+
+14
+
+print("Division=",div)
+mod=num1%num2
+print("Modulus=",mod)
