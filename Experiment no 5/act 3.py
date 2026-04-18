5e80d72e675ec7714f613308bacafb72da7a065d
rajmagar5135 <magarraj45426@gmail.com>
2026-04-18 13:01:09 +0700

    Create act 3.py

---
+++ Experiment no 5/act 3.py
@@ -0,0 +1,20 @@
+# Add new stock in shop inventory
+Created on Mon Mar 9 14:45:31 2026
+@author: Raj Magar
+inventory = {}
+
+
+inventory["pen"] = 15
+
+
+
+inventory["notebook"] = 20
+inventory["pencil"] = 9
+
+
+
+print("Original inventory.", inventory)
+print(inventory)
+inventory["eraser"] = 7
+print("\nUpdated inventory:")
+print(inventory)
