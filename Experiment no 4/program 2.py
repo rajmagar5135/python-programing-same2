94ac2e1a551abf9b9b1cb1b7d5e34e9e43a844d9
rajmagar5135 <magarraj45426@gmail.com>
2026-04-18 12:42:18 +0700

    Create program 2.py

---
+++ Experiment no 4/program 2.py
@@ -0,0 +1,14 @@
+#Take input from the user
+Created on Fri Mar 27 23:07:16 2026
+@author: Raj Magar
+string = input("Enter a string:")
+
+
+
+# Reverse the string using slicing
+reversed_string = string[ :: -1]
+# Print the result
+
+
+
+print("Reversed string:", reversed_string)
