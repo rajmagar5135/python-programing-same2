eab22da1d72629944ec897a0907a96684979adf3
rajmagar5135 <magarraj45426@gmail.com>
2026-04-18 12:36:29 +0700

    Create act 3.py

---
+++ Experiment no 3/act 3.py
@@ -0,0 +1,15 @@
+# Input salary from user
+Created on Fri Mar 27 21:30:33 2026
+@author: Raj Magar
+salary = float(input("Enter your salary: "))
+# Check loan approval
+if salary >= 25000:
+print("Loan Approved")
+
+
+
+lse:
+
+
+
+print("Loan Rejected")
