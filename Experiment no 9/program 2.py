7eb3bceb28d680e02d809a3650a10deb15cbdcc6
rajmagar5135 <magarraj45426@gmail.com>
2026-04-21 10:13:18 +0530

    Create program 2.py

---
+++ Experiment no 9/program 2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 20 09:17:24 2026
+
+@author: Nikhil Nikam
+"""
+
+import datetime
+# Get current date and time
+now = datetime.datetime.now()
+# Print current date
+print("Current Date:", now.strftime("%Y-%m-%d"))
+# Print current time
+print("Current Time:", now.strftime("%H:%M:%S"))
+
+# Print current weekday
+print("Weekday:", now.strftime("%A"))
